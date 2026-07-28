a65907bdfafcc60da9f8ce5207fdddc086c3a03b
LCZW973 <leonardoczanin@gmail.com>
2026-07-28 19:11:24 -0300

    Update Ex2.py

--- ExDeEntrega/Cap.2/Parte1/Ex2.py
+++ ExDeEntrega/Cap.2/Parte1/Ex2.py
@@ -1,1 +1,6 @@
-
+numero_selecionado = int(input('Numero selecionada para tabuada : '))
+intervalo_inicial = int(input('Intervalo inicial : '))
+intervalo_final = int(input('Intervalo final : '))
+intervalo_final += 1
+for c in range(intervalo_inicial,intervalo_final):
+    print(c*numero_selecionado)
