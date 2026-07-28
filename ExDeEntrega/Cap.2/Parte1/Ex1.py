cbbf64c7c698124079080998d22408864862dc8c
LCZW973 <leonardoczanin@gmail.com>
2026-07-28 16:56:56 -0300

    Update Ex1.py

--- ExDeEntrega/Cap.2/Parte1/Ex1.py
+++ ExDeEntrega/Cap.2/Parte1/Ex1.py
@@ -1,8 +1,20 @@
 Nome = input('Nome completo : ')
-#padronizacao do nome para evitar erros de comparacao
-Nome = Nome.upper()
 
-print('Nome com letras maiusculas : ',Nome)
+print('Nome com letras maiusculas : ',Nome.upper())
 print('Nome com letras minusculas : ',Nome.lower())
-print('Quantas letras tem em seu nome : ',len(Nome))
-print('Trocando o ultimo nome por outra palavra : ',Nome.replace('ZANIN','do Inatel'))
+
+#for each que varre a str buscando espacos para marcar o inicio da ultima parte do nome e a quantidade de espacos em branco
+Tamanho = len(Nome)
+MarcadorInicio = 0 ;
+Vazio = 0 ;
+for C in range (0 ,Tamanho) :
+    if Nome[C] == ' ' :
+       MarcadorInicio  = C
+       Vazio += 1
+
+Tamanho -= Vazio
+print('Quantas letras tem em seu nome : ',Tamanho)
+
+Nome = Nome[:MarcadorInicio] + ' do Inatel'
+print('Trocando o ultimo nome por outra palavra : ',Nome)
+
