52a3342fc3e31b5fd451b0162746bad9f91d1237
LCZW973 <leonardoczanin@gmail.com>
2026-07-28 15:18:17 -0300

    Update Ex7.py

--- ExDeEntrega/Cap.2/Parte3/Ex7.py
+++ ExDeEntrega/Cap.2/Parte3/Ex7.py
@@ -1,1 +1,21 @@
+palavra = input("digite uma palavra : ")
+palavra = palavra.upper()
+contador = 0 
+detectaA = 0
+
+for c in range (0,len(palavra)) :
+    if palavra[c] =='A' :
+       contador+=1
+       detectaA+=1
+    if palavra[c]=='E' :
+       contador+=1
+    if palavra[c]=='I' :
+       contador+=1
+    if palavra[c]=='O' :
+       contador+=1
+    if palavra[c]=='U' :
+       contador+=1
+
+print('O numero de vogais sao : ',contador)
+print('O numero de letras A sao : ',detectaA)
 
