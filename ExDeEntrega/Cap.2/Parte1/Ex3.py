2408ab727356117f3411ed6838634c865aff84cc
LCZW973 <leonardoczanin@gmail.com>
2026-07-28 19:20:06 -0300

    Update Ex3.py

--- ExDeEntrega/Cap.2/Parte1/Ex3.py
+++ ExDeEntrega/Cap.2/Parte1/Ex3.py
@@ -1,5 +1,10 @@
 while True:
- sexo = input("digite M (masculino) ou F (feminino) : ")
- sexo =sexo.upper()
- if sexo == 'M' or sexo == 'F' :
-    break;
+  sexo = input("digite M (masculino) ou F (feminino) : ")
+  sexo =sexo.upper()
+  if sexo == 'M' or sexo == 'F' :
+     break;
+
+if sexo == 'M' :
+   print('Homem')
+else :
+   print('Mulher')
