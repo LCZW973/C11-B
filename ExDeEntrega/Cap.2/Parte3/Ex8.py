184803b0b7d65477df1feaa6722873439621561f
LCZW973 <leonardoczanin@gmail.com>
2026-07-28 15:36:37 -0300

    Create Ex8.py

---
+++ ExDeEntrega/Cap.2/Parte3/Ex8.py
@@ -0,0 +1,9 @@
+FirstNumber = int(input('Primeiro numero a ser inserido : '))
+SecondNumber = int(input('Segundo numero a ser inserido : '))
+
+print ('Soma : ',FirstNumber+SecondNumber)
+print('Subtracao : ',FirstNumber-SecondNumber)
+print('Multiplicacao : ',FirstNumber*SecondNumber)
+print('Divisao : ',FirstNumber/SecondNumber)
+print('Resto da divisao : ',FirstNumber%SecondNumber)
+print('Potencia : ',FirstNumber**SecondNumber)
