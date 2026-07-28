451fc75f0903087527552b0381d512ea5964cd5e
LCZW973 <leonardoczanin@gmail.com>
2026-07-28 15:51:41 -0300

    Update Ex6.py

--- ExDeEntrega/Cap.2/Parte3/Ex6.py
+++ ExDeEntrega/Cap.2/Parte3/Ex6.py
@@ -1,1 +1,7 @@
+import math
+Numero = float(input('Inserir numero decimal : '))
 
+print('Raiz quadrada do numero ',Numero**(0.5))
+print('Arredondamento teto : ',math.ceil(Numero))
+print('Arredondamento chao : ',math.floor(Numero))
+print('Truncamento para inteiro : ',math.trunc(Numero))
