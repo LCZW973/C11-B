5d2793da190d278e91ac9009d86d1bce250d01c9
LCZW973 <leonardoczanin@gmail.com>
2026-07-28 16:13:36 -0300

    Update Ex5.py

--- ExDeEntrega/Cap.2/Parte2/Ex5.py
+++ ExDeEntrega/Cap.2/Parte2/Ex5.py
@@ -1,1 +1,10 @@
+while True :
+  Numero = int(input('Insira um valor entre 1000 e 9999 : '))
+  if Numero > 1000 and Numero < 9999 :
+     break ; 
+  print('O numero inserido esta no intervalo errado')
 
+print('O numero da unidade : ',Numero%10)  
+print('O numero da dezena : ',(Numero%100-Numero%10)//10)
+print('O numero da centena : ',(Numero%1000-Numero%100)//100)
+print('O numero do milhar',(Numero%10000-Numero%1000)//1000)
