c4595b52ee3b4916330ffc87922cf6876797a2a4
LCZW973 <leonardoczanin@gmail.com>
2026-08-03 23:17:37 -0300

    Update Questao4.py

--- ExDeEntrega/Cap3/Parte2/Questao4.py
+++ ExDeEntrega/Cap3/Parte2/Questao4.py
@@ -1,1 +1,19 @@
+pessoa1={'nome':'leo' ,'peso':70}
+pessoa2={'nome':'andrey' ,'peso':60}
+pessoa3={'nome':'lucas' , 'peso':72}
 
+pessoas = [pessoa1, pessoa2 ,pessoa3]
+
+max_peso = pessoas[0]['peso']
+min_peso = max_peso
+
+for c in range ( 0 , len(pessoas)) :
+   if pessoas[c]['peso'] > max_peso :
+      max_peso = pessoas[c]['peso']
+      max_peso_nome = pessoas[c]['nome']
+   if pessoas[c]['peso'] <min_peso :
+      min_peso = pessoas[c]['peso']
+      min_peso_nome = pessoas[c]['nome']
+
+print('Mais pesada : ' , max_peso_nome)
+print('Menos pesada : ',min_peso_nome)      
