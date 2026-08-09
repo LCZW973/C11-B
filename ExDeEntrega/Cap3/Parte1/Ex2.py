426b4ee997f3895f994ed9f28d3b547c41b3f017
LCZW973 <leonardoczanin@gmail.com>
2026-08-09 19:46:47 -0300

    Update Ex2.py

--- ExDeEntrega/Cap3/Parte1/Ex2.py
+++ ExDeEntrega/Cap3/Parte1/Ex2.py
@@ -1,19 +1,11 @@
 loja_one = {'Galaxy A37','Galaxy A17','Galaxy A16','Galaxy S26','Galaxy S25'}
 loja_two = {'Galaxy A37','Galaxy A17','Galaxy A16','Galaxy Z Fold'}
 
-print('modelos da loja A')
-for c in loja_one :
-  print(c)
-
-print('modelos da loja B')
-for c in loja_two :
-  print(c)
-
-print('total de modelos disponiveis : ',len(loja_one|loja_two))
-
+#mostra os modelos da loja
+print("modelos da loja A : ",loja_one)
+print("modelos da loja B : ",loja_two)
+#numero de modelos distintos
+print('total de modelos disponiveis : ',len(loja_one |loja_two))
+#modelos em ambas lojas
 modelos_semelhantes = loja_one & loja_two
-
-print('modelos semelhantes : ')
-for c in modelos_semelhantes :
-      print(c)
-      
+print('modelos semelhantes : ',modelos_semelhantes)
