d04d45ba0f25916613632a05924938a1251e3b6b
LCZW973 <leonardoczanin@gmail.com>
2026-08-14 17:07:52 -0300

    Update Ex1.py

--- ExDeEntrega/Cap4/Parte1/Ex1.py
+++ ExDeEntrega/Cap4/Parte1/Ex1.py
@@ -1,1 +1,13 @@
+import numpy as np 
 
+lista = np.ones([1,8])
+
+listadois = np.random.randint(0,10,8)
+
+listatres = lista+listadois
+if(listatres.sum(axis=1)>=40) :
+    listatres= listatres.reshape(4,2)
+else :
+    listatres= listatres.reshape(2,4)
+    
+print(listatres)
