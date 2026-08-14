8ad9befec377822877909bc7f0c313736b3948b6
LCZW973 <leonardoczanin@gmail.com>
2026-08-14 16:55:58 -0300

    Update Ex4.py

--- ExDeEntrega/Cap4/Parte2/Ex4.py
+++ ExDeEntrega/Cap4/Parte2/Ex4.py
@@ -1,1 +1,14 @@
+import numpy as np 
 
+mtz = np.arange(0,4,1)
+mtz = mtz.reshape(2,2)
+
+
+linha,coluna = mtz.shape
+
+total = linha*coluna
+
+if(total%2 == 0) :
+    print('par')
+else :
+    print('impar')
