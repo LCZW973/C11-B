b78a0af08f9d2aefc09a022b3d360d7a8cb07589
LCZW973 <leonardoczanin@gmail.com>
2026-08-17 17:50:22 -0300

    Update aula2.py

--- Aula/CAP3/aula2.py
+++ Aula/CAP3/aula2.py
@@ -1,1 +1,11 @@
+import numpy as np 
+#numpy nao trabalha com elementos heterogeneos
 
+  '''Slice no Numpy'''
+#plantando a mesma semente aleatoria
+np.random.seed(10)
+#estruturando a matriz 3 x 3
+mtz = np.random.randint(1,99,9).reshape(3,3)
+print(mtz)
+
+  
