5c36959b7132dd89b6e5b22abac29297d2bf8c9d
LCZW973 <leonardoczanin@gmail.com>
2026-08-17 18:10:20 -0300

    Update aula2.py

--- Aula/CAP3/aula2.py
+++ Aula/CAP3/aula2.py
@@ -1,11 +1,25 @@
 import numpy as np 
 #numpy nao trabalha com elementos heterogeneos
 
-  '''Slice no Numpy'''
+#Slice no Numpy
 #plantando a mesma semente aleatoria
 np.random.seed(10)
 #estruturando a matriz 3 x 3
 mtz = np.random.randint(1,99,9).reshape(3,3)
 print(mtz)
+print('extraindo apenas a segunda linha da matriz')
+print(mtz[1])
+print('extraindo a terceira coluna da matriz')
+print(mtz[:,2])
+print('extraindo a matriz 2x2 no canto inferior direito da matriz')
+print(mtz[1:,1:])
+
+#condicionais no Numpy
+print('mostre apenas os elementos menores que setenta')
+print(mtz<70)
+print(mtz[mtz<70])
+print('dessa matriz retorne apenas os numeros pares')
+print(mtz%2==0)
+print(mtz[mtz%2==0])
 
   
